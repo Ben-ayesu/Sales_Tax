8b6c38b7129c1cdd1deaf8305bcaa14b3920c1d2
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2022-12-21 21:19:11 -0800

    Initial Release to app store

---
+++ app/src/main/java/salestaxcalculator/example/bcsalestax/MainActivity.kt
@@ -0,0 +1,226 @@
+@file:Suppress("OPT_IN_IS_NOT_ENABLED")
+
+package salestaxcalculator.example.bcsalestax
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.*
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.bcsalestax.R
+import salestaxcalculator.salestax.bcsalestax.ui.theme.BCSalesTaxTheme
+import java.text.NumberFormat
+import androidx.compose.material3.Icon as Material3Icon
+
+val itemPrice = mutableStateOf("")
+val govTax = mutableStateOf("")
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BCSalesTaxTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScreen() {
+    //Outlined Item price input
+    var enterItemPrice by remember { mutableStateOf("") }
+    //Outlined Tax Rate input
+    var enterTax by remember { mutableStateOf("") }
+
+    //Calculations
+    val amount = enterItemPrice.toDoubleOrNull() ?: 0.0
+    val tax = enterTax.toDoubleOrNull() ?: 0.0
+
+    val tip = calculateTaxesTotal(amount, tax)
+
+    Scaffold(
+        topBar = {
+            SalesTopAppBar()
+        }
+    ) {
+        Column(
+            Modifier
+                .padding(top = 25.dp)
+                .fillMaxSize()
+        ) {
+            //Calculate Title Text
+            Text(
+                stringResource(id = R.string.calculate),
+                Modifier
+                    .fillMaxWidth()
+                    .padding(start = 8.dp, end = 8.dp, top = 8.dp),
+                fontSize = 45.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+            //Tax Text
+            Text(
+                stringResource(id = R.string.taxes),
+                Modifier
+                    .fillMaxWidth()
+                    .padding(start = 8.dp, end = 8.dp),
+                fontSize = 45.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+            //Enter Item Price Text Field
+            EditItemNumberField(
+                enterItemPrice,
+                onValueChange = { enterItemPrice = it }
+            )
+            //Enter Tax Rate Text Field
+            EditTaxRate(
+                value = enterTax,
+                onValueChange = { enterTax = it })
+
+            //Calculate Button
+            CalculateButton()
+
+            //Show tax amount results
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    stringResource(id = R.string.tax_amount, tip),
+                    Modifier
+                        .padding(8.dp),
+                    fontSize = 25.sp
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun SalesTopAppBar(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .size(width = 0.dp,height = 40.dp)
+            .background(color = MaterialTheme.colors.primary),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            stringResource(id = R.string.app_name)
+        )
+    }
+}
+
+
+@Composable
+fun EditItemNumberField(
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp, top = 8.dp),
+        label = { Text("Enter the Item Price") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        maxLines = 1,
+        leadingIcon = { Text(text = "$") },
+        trailingIcon = {
+            IconButton(onClick = {
+                itemPrice.value = " "
+            }) {
+                Material3Icon(imageVector = Icons.Default.Close, contentDescription = "Close")
+            }
+        },
+        shape = CircleShape
+    )
+}
+
+@Composable
+fun EditTaxRate(
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp, top = 8.dp),
+        label = { Text(text = "Enter Tax Rate") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        maxLines = 1,
+        leadingIcon = { Text(text = "%") },
+        trailingIcon = {
+            IconButton(onClick = {
+                govTax.value = ""
+            }) {
+                Material3Icon(imageVector = Icons.Default.Clear, contentDescription = "Clear")
+            }
+        },
+        shape = CircleShape
+    )
+}
+
+@Composable
+fun CalculateButton() {
+    OutlinedButton(
+        onClick = {
+
+        },
+        Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, top = 8.dp, end = 8.dp),
+        elevation = ButtonDefaults.elevatedButtonElevation(
+            defaultElevation = 4.dp,
+            pressedElevation = 8.dp
+        ),
+        shape = CircleShape,
+        contentPadding = PaddingValues(12.dp)
+    ) {
+        Text(text = "Calculate")
+    }
+}
+
+private fun calculateTaxesTotal(
+    amount: Double,
+    taxRate: Double
+): String {
+    val tip = taxRate / 100 * amount
+    return NumberFormat.getCurrencyInstance().format(tip)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BCSalesTaxTheme {
+        MainScreen()
+    }
+}
