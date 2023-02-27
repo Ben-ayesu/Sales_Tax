9859bb7de0f9288889651eff88d43781f0b61c60
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-02-26 21:02:14 -0800

    Working

--- app/src/main/java/salestaxcalculator/example/bcsalestax/Screen.kt
+++
@@ -1,6 +0,0 @@
-package salestaxcalculator.example.bcsalestax
-
-sealed class Screen(val route: String) {
-    object Main: Screen(route = "main_screen")
-    object Budget: Screen(route = "budget_screen")
-}
